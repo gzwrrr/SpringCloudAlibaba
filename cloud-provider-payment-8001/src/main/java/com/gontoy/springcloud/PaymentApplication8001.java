6e5c10e368c8ebe7bcf197ec615f2138c8cf09b0
gzw <1627121193@qq.com>
2022-08-18 21:26:18 +0800

    init

---
+++ cloud-provider-payment-8001/src/main/java/com/gontoy/springcloud/PaymentApplication8001.java
@@ -0,0 +1,15 @@
+package com.gontoy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 主启动类
+ * @author gzw
+ */
+@SpringBootApplication
+public class PaymentApplication8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication8001.class, args);
+    }
+}
